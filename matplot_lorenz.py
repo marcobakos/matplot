5b491e93478616d4efe8c66d4240e9dd9f29dd9e
marcobakos <marco.bakos@gmail.com>
2018-12-27 19:12:21 -0500

    fixed and it is working now

--- matplot_lorenz.py
+++ matplot_lorenz.py
@@ -1,11 +1,11 @@
-# %matplotlib inline
-
-from ipywidgets import interact, interactive
-from IPython.display import clear_output, display, HTML
-
 import numpy as np
-from scipy import integrate
+import matplotlib
 
+from scipy import integrate
+from ipywidgets import interactive
+from IPython.display import display
+
+matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import cnames
@@ -14,7 +14,11 @@ from matplotlib import animation
 print('Inicio do programa')
 
 
-def solve_lorenz(N=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.0):
+def solve_lorenz(n=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.0):
+    """
+
+    :type max_time: object
+    """
     fig = plt.figure();
     ax = fig.add_axes([0, 0, 1, 1], projection='3d');
     # ax.axis('off')
@@ -31,7 +35,7 @@ def solve_lorenz(N=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.
 
     # Choose random starting points, uniformly distributed from -15 to 15
     np.random.seed(1)
-    x0 = -15 + 30 * np.random.random((N, 3))
+    x0 = -15 + 30 * np.random.random((n, 3))
 
     # Solve for the trajectories
     t = np.linspace(0, max_time, int(250 * max_time))
@@ -39,10 +43,10 @@ def solve_lorenz(N=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.
                       for x0i in x0])
 
     # choose a different color for each trajectory
-    colors = plt.cm.jet(np.linspace(0, 1, N));
+    colors = plt.cm.jet(np.linspace(0, 1, n));
 
     print("criando linhas")
-    for i in range(N):
+    for i in range(n):
         print(f"index: {i}")
         x, y, z = x_t[i, :, :].T
         lines = ax.plot(x, y, z, '-', c=colors[i])
@@ -52,7 +56,7 @@ def solve_lorenz(N=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.
     ax.view_init(30, angle)
 
     print("Usando Interactive ")
-    w = interactive(solve_lorenz, angle=(0., 360.), N=(0, 50), sigma=(0.0, 50.0), rho=(0.0, 50.0))
+    w = interactive(solve_lorenz, angle=(0., 360.), n=(0, 50), sigma=(0.0, 50.0), rho=(0.0, 50.0))
     display(w)
 
     x1 = plt.isinteractive()
@@ -65,8 +69,8 @@ def solve_lorenz(N=10, angle=0.0, max_time=4.0, sigma=10.0, beta=8. / 3, rho=28.
 # execucao
 
 print('Loading lorenz')
-t, x_t = solve_lorenz(angle=0, N=10)
+t, x_t = solve_lorenz(angle=0, n=10)
 
 # print("Usando Interactive ")
-# w = interactive(solve_lorenz, angle=(0., 360.), N=(0, 50), sigma=(0.0, 50.0), rho=(0.0, 50.0))
+# w = interactive(solve_lorenz, angle=(0., 360.), n=(0, 50), sigma=(0.0, 50.0), rho=(0.0, 50.0))
 # display(w)
